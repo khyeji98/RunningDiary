8491e5bd7c5caf7b7c7d119808b091e43a0356d5
khyeji98 <gpwl98wkd@gmail.com>
2026-06-20 03:38:54 +0900

    feat: AuthSession에 refreshToken 필드 추가 (#23)
    토큰 갱신을 위한 refresh token을 옵셔널로 보관

--- Dependencies/Models/Sources/Models/Entities/AuthSession.swift
+++ Dependencies/Models/Sources/Models/Entities/AuthSession.swift
@@ -9,21 +9,29 @@
 public struct AuthSession: Equatable, Sendable, Codable {
     public let accessToken: String
     public let tokenType: String
+    /// accessToken 만료 시 갱신에 사용하는 refresh token.
+    ///
+    /// 현재 서버가 반환하지 않을 수 있어 옵셔널로 둔다.
+    /// TODO: 서버가 refresh_token을 반환하기 시작하면 옵셔널 해제를 검토한다.
+    public let refreshToken: String?
     public let user: AuthUser
 
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
         case tokenType = "token_type"
+        case refreshToken = "refresh_token"
         case user
     }
 
     public init(
         accessToken: String,
         tokenType: String,
-        user: AuthUser
+        user: AuthUser,
+        refreshToken: String? = nil
     ) {
         self.accessToken = accessToken
         self.tokenType = tokenType
         self.user = user
+        self.refreshToken = refreshToken
     }
 }
